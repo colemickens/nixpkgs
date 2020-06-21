a090a12f210025d24345eed68f8568e7e0691905
Cole Mickens <cole.mickens@gmail.com>
2020-06-20 17:49:56 -0700

    pythonPackages.cssselect2: repair test exemptions, fetchFromGH

--- pkgs/development/python-modules/cssselect2/default.nix
+++ pkgs/development/python-modules/cssselect2/default.nix
@@ -1,28 +1,28 @@
-{ lib, buildPythonPackage, fetchPypi, tinycss2, pytest, pytestrunner }:
+{ lib, buildPythonPackage, fetchFromGitHub, tinycss2, pytest, pytestrunner }:
 
 buildPythonPackage rec {
   pname = "cssselect2";
   version = "0.3.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "5c2716f06b5de93f701d5755a9666f2ee22cbcd8b4da8adddfc30095ffea3abc";
+  src = fetchFromGitHub {
+    owner = "Kozea";
+    repo = "cssselect2";
+    rev = "${version}";
+    sha256 = "04hswa61h1dyaj0lsmvasj9h1zjsrmg6i7k4ki2ssh35lyf9m489";
   };
 
   # We're not interested in code quality tests
   postPatch = ''
-    substituteInPlace setup.py \
+    substituteInPlace setup.cfg \
       --replace "pytest-cov" "" \
       --replace "pytest-flake8" "" \
       --replace "pytest-isort" ""
     substituteInPlace setup.cfg \
-      --replace "--cov=cssselect2" "" \
       --replace "--flake8" "" \
       --replace "--isort" ""
   '';
 
   propagatedBuildInputs = [ tinycss2 ];
-
   checkInputs = [ pytest pytestrunner ];
 
   meta = with lib; {
